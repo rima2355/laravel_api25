a7977f4a51b7e02eb16c1861bbf2851897b516f5
Faisaleka21 <162295533+Faisaleka21@users.noreply.github.com>
2025-10-24 22:18:29 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,4 @@ Route::get('/', function () {
 });
 
 Route::get('/welcome', [WelcomeController::class, 'index']);
+Route::get('/book', [BookController::class, 'index']);
